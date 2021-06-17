b5c8b3ccd51ce68b6ec80ed6edef2de942d7e8e0
Franco <perez_salazarf@hotmail.com>
2021-06-17 17:06:01 -0500

    boton derecho clickear en todo el document

--- pag.js
+++ pag.js
@@ -1,12 +1,34 @@
 const nombre= document.getElementById('alum')
 const button= document.querySelector('button')
-const dato= e => {
-    alert(e.target.textContent)
+// const dato= e => alert(e.target.textContent)
+
+// nombre.addEventListener('dblclick',e => {
+//     dato(e)
+// })
+// button.addEventListener('click',e =>{
+//     dato(e)
+// })
+
+const creaMenu= e=> {
+    const menu= document.createElement('div')
+    const prevMenu= document.getElementById('menuu')
+    menu.setAttribute('id','menuu')
+    menu.textContent= 'document'
+
+    if(prevMenu){
+        document.body.removeChild(prevMenu)
+    }
+
+    document.body.appendChild(menu)
+
+    menu.style.background= '#eee'
+    menu.style.padding= '1em'
+    menu.style.position= 'fixed'
+    menu.style.top= `${e.pageY}px`
+    menu.style.left= `${e.pageX}px`
 }
 
-nombre.addEventListener('dblclick',e => {
-    dato(e)
-})
-button.addEventListener('click',e =>{
-    dato(e)
+document.addEventListener('contextmenu', e => {
+    creaMenu(e)
+    e.preventDefault()
 })
