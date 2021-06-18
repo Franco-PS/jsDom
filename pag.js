d3db4b3f0564e646b500306ce732b13651cfee68
Franco <perez_salazarf@hotmail.com>
2021-06-17 19:53:23 -0500

    mostrar alerta con las teclas ctrl + a

--- pag.js
+++ pag.js
@@ -1,34 +1,8 @@
-const nombre= document.getElementById('alum')
-const button= document.querySelector('button')
-// const dato= e => alert(e.target.textContent)
+const input= document.getElementById('input')
 
-// nombre.addEventListener('dblclick',e => {
-//     dato(e)
-// })
-// button.addEventListener('click',e =>{
-//     dato(e)
-// })
-
-const creaMenu= e=> {
-    const menu= document.createElement('div')
-    const prevMenu= document.getElementById('menuu')
-    menu.setAttribute('id','menuu')
-    menu.textContent= 'document'
-
-    if(prevMenu){
-        document.body.removeChild(prevMenu)
+input.addEventListener('keyup',e => {
+    if(e.ctrlKey === true && e.key === 'a'){
+        alert('User CTRL A')
     }
-
-    document.body.appendChild(menu)
-
-    menu.style.background= '#eee'
-    menu.style.padding= '1em'
-    menu.style.position= 'fixed'
-    menu.style.top= `${e.pageY}px`
-    menu.style.left= `${e.pageX}px`
-}
-
-document.addEventListener('contextmenu', e => {
-    creaMenu(e)
-    e.preventDefault()
 })
+
