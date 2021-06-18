41c8af956f52be9c61243f48a2c2eca9ed7f67ae
Franco <perez_salazarf@hotmail.com>
2021-06-18 00:55:44 -0500

    Juego de laberinto tecleando Arrow/up/down/left/right

--- pag.js
+++ pag.js
@@ -1,8 +1,46 @@
-const input= document.getElementById('input')
 
-input.addEventListener('keyup',e => {
-    if(e.ctrlKey === true && e.key === 'a'){
-        alert('User CTRL A')
+let x=0,y=0
+addEventListener('keyup',e=>{
+    const ed= document.getElementById('unico')
+    console.log(e.key)
+    
+    const move= () => ed.style.transform= `translate(${x*20}px,${y*20}px)`
+    const up= ()=> {
+        y--
+        move()
+        console.log(x,y)
+    }
+    const down= ()=> {
+        y++
+        move()
+        console.log(x,y)
+    }
+    const left= ()=> {
+        x--
+        move()
+        console.log(x,y)
+    }
+    const right= ()=> {
+        x++
+        move()
+        console.log(x,y)
+    }
+
+    switch (e.key){
+        case 'ArrowUp':
+            up()
+            break;
+        case 'ArrowDown':
+            down()
+            break;
+        case 'ArrowLeft':
+            left()
+            break;
+        case 'ArrowRight':
+            right()
+            break;
+        default:
+            break;
+
     }
 })
-
